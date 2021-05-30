7dc885c5a2d9be3b5378c745ba646e3022fd447e
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-05-30 21:33:06 +0800

    Update jaura.js

--- jaura.js
+++ jaura.js
@@ -2,10 +2,10 @@ var urlmain = document.URL;
 var param = document.URL.split("/")[3].replace("?m=1","").replace("?m=0","");
 var dataUrl = [
 
-"https://dev-neomoviefj.pantheonsite.io/",
+"https://dev-neomoviefj.pantheonsite.io//movies/arafar",
 "https://dev-filmflixss.pantheonsite.io/",
-"https://dev-neoflimfj.pantheonsite.io/",
-"https://dev-neofilmfj.pantheonsite.io/",
+"https://dev-neoflimfj.pantheonsite.io//movies/tvmovies",
+"https://dev-neofilmfj.pantheonsite.io/movies/search",
 "https://dev-filmfj.pantheonsite.io/"
 
 ];
